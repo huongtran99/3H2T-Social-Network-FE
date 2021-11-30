aacb150900f3f03366d4a60e72979bb5a59f0f92
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2021-11-30 14:13:37 +0700

    add auth & admin module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PostRoutingModule } from './post-routing.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PostRoutingModule
+    AuthRoutingModule
   ]
 })
-export class PostModule { }
+export class AuthModule { }
